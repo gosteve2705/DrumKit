f014cfc070e99bd6f0a45786751c5352c8a7671a
gosteve <nomuojamaro@gmail.com>
2020-05-31 02:41:01 +0100

    starting files of my drum kit website

---
+++ index.js
@@ -0,0 +1,32 @@
+
+var length = document.querySelectorAll('.drum').length;
+for(var i = 0 ; i < length; i++ ){
+
+document.querySelectorAll('.drum')[i].addEventListener('click',function (){
+  if(this.innerHTML === 'w'){
+    var audio =  new Audio('sounds/tom-1.mp3');
+    audio.play();
+  }
+  else if (this.innerHTML === 'a'){
+    var audio =  new Audio('sounds/tom-2.mp3');
+    audio.play();
+  }else if (this.innerHTML === 's'){
+    var audio =  new Audio('sounds/tom-3.mp3');
+    audio.play();
+  }else if (this.innerHTML === 'd'){
+    var audio =  new Audio('sounds/kick-bass.mp3');
+    audio.play();
+  }else if (this.innerHTML === 'j'){
+    var audio =  new Audio('sounds/crash.mp3');
+    audio.play();
+  }else if (this.innerHTML === 'k'){
+    var audio =  new Audio('sounds/snare.mp3');
+    audio.play();
+  }else if (this.innerHTML === 'l'){
+    var audio =  new Audio('sounds/tom-4.mp3');
+    audio.play();
+  }
+
+});
+
+}
